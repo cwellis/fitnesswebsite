561cc22e4a10ba61f7cf2e76c65e4ee5d296dfcd
cwellis <cwellis2016@gmail.com>
2022-10-25 07:11:49 -0400

    figures section added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -19,13 +19,33 @@ const Hero = () => {
 
                 <div className='hero-text'>
                     <div>
-                        <span>Shape </span>
+                        <span className='stroke-text'>Shape </span>
                         <span>Your</span>
                     </div>
                     <div>
                         <span>Ideal Body</span>
                     </div>
+                    <div className='ideal'>
+                        <span>In here we will help you to shape and build your ideal body and live your life to the fullest</span>
+                    </div>
 
+                </div>
+
+                {/* figures */}
+
+                <div className='figures'>
+                    <div>
+                        <span>140+</span>
+                        <span>Expert Coaches</span>
+                    </div>
+                    <div>
+                        <span>978+</span>
+                        <span>Members Joined</span>
+                    </div>
+                    <div>
+                        <span>50+</span>
+                        <span>Fitness Programs</span>
+                    </div>
                 </div>
 
 
